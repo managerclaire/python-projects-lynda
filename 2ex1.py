7190a68cb646ebb0fef8bd7865c95acc314dc931
Claire R <clairepanak@gmail.com>
2017-12-01 17:01:24 +0000

    self

--- 2ex1.py
+++ 2ex1.py
@@ -1,9 +1,15 @@
 class PythonProjects():
+    def __init__(self, msg):
+        self.msg = msg
+        
     def say_hello(self):
-        print("Hello Python")
+        print(self.msg)
+        
+if __name__ == "__main___":
+    print("Hi")
 
 #instance of class
-inst = PythonProjects()
-
+inst = PythonProjects("First message")
+    
 #call via instance
 inst.say_hello()
